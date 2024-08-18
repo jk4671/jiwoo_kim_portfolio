aaccb3f38f9611b9bb1a53c81738f46aa84db68f
jk4671 <jk4671@columbia.edu>
2024-08-17 23:40:15 -0500

    edited vite deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.glb']
-})
+  assetsInclude: ['**/*.glb'],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'], // Create a vendor chunk for libraries
+          // Define other chunks as needed
+        }
+      }
+    },
+    chunkSizeWarningLimit: 2000 // Increase limit if you don't want warnings
+  }
+});
